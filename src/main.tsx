15aca9bd346954f2a52b7b2182dd63f47300d824
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-07-29 14:26:22 +0300

    Load scroll hero styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './header-contrast.css';
+import './scroll-hero.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
